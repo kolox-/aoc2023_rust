53b59035bf472a5b25d27c39303bd15041e065d2
kolox- <colinbutler1995@gmail.com>
2023-12-04 00:14:55 +0000

    Update to remove previous code day1

--- day01/src/bin/part1.rs
+++ day01/src/bin/part1.rs
@@ -7,24 +7,6 @@ fn main() {
     dbg!(p2_output);
 }
 
-// fn get_calibration(string: &str) -> u32 {
-//     let mut first: Option<char> = None;
-//     let mut last: char = ' ';
-//     for ch in string.chars() {
-//         if ch.is_numeric() {
-//             last = ch;
-//             if first.is_none() {
-//                 first = Some(ch);
-//             }
-//         }
-//     }
-//     if first.is_none() {
-//         panic!("Didn't find a digit: {}", string.to_string());
-//     }
-//     let answer = first.unwrap().to_digit(10).unwrap() * 10 + last.to_digit(10).unwrap();
-//     answer
-// }
-
 fn get_calibration(string: &str) -> u32 {
     let digits: Vec<u32> = string.chars().filter_map(|c| c.to_digit(10)).collect();
     digits[0] * 10 + digits[digits.len() - 1]
@@ -42,39 +24,21 @@ fn numerize(line: &str) -> String {
         .replace("nine", "n9ine")
 }
 
-fn part1(input: &str) -> String {
-    // let mut count: u32 = 0;
-    // let v: Vec<&str> = input.split('\n').collect();
-    // for line in v {
-    //     if !line.is_empty() {
-    //         count += get_calibration(line);
-    //     }
-    // }
-    // count.to_string()
-
+fn part1(input: &str) -> u32 {
     input
         .lines()
         .filter(|s| !s.is_empty())
         .map(|s| get_calibration(s))
         .sum::<u32>()
-        .to_string()
 }
 
-fn part2(input: &str) -> String {
-    // let mut count: u32 = 0;
-    // for line in input.split('\n') {
-    //     if !line.is_empty() {
-    //         count += get_calibration(&numerize(line));
-    //     }
-    // }
-    // count.to_string()
+fn part2(input: &str) -> u32 {
     input
         .lines()
         .filter(|s| !s.is_empty())
         .map(|s| numerize(s))
         .map(|s| get_calibration(&s))
         .sum::<u32>()
-        .to_string()
 }
 
 #[cfg(test)]
@@ -89,7 +53,7 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet",
         );
-        assert_eq!(result, "142".to_string());
+        assert_eq!(result, 142);
     }
 
     #[test]
@@ -103,13 +67,13 @@ xtwone3four
 zoneight234
 7pqrstsixteen",
         );
-        assert_eq!(result, "281".to_string());
+        assert_eq!(result, 281);
     }
 
     #[test]
     fn day1_full_input() {
         let input = include_str!("../../../inputs/1.txt");
-        assert_eq!(part1(input), "53386".to_string());
-        assert_eq!(part2(input), "53312".to_string());
+        assert_eq!(part1(input), 53386);
+        assert_eq!(part2(input), 53312);
     }
 }
